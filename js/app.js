429a9d4d3766831c1d712d3187fa0b1ec95fb657
sok213 <sophanarithsok@gmail.com>
2016-06-18 06:28:28 -0700

    add back http to URL api links

--- js/app.js
+++ js/app.js
@@ -126,7 +126,7 @@ $(function () {
             async: true,
             type: 'GET',
             data: params,
-            url: "https://dev.markitondemand.com/Api/v2/InteractiveChart/jsonp",
+            url: "http://dev.markitondemand.com/Api/v2/InteractiveChart/jsonp",
             dataType: "jsonp",
             context: this,
             success: function(json){
@@ -373,7 +373,7 @@ $(function () {
         $.ajax({
             async: true,
             type: 'GET',
-            url: "https://dev.markitondemand.com/MODApis/Api/v2/Lookup/jsonp?input="+userInput,
+            url: "http://dev.markitondemand.com/MODApis/Api/v2/Lookup/jsonp?input="+userInput,
             dataType: "jsonp",
             context: this,
             success: function(json){
