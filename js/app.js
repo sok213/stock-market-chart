9b1cba1111989cf7609acc82b4f71d91fc23e529
sok213 <sophanarithsok@gmail.com>
2016-06-27 20:54:46 -0700

    test

--- js/app.js
+++ js/app.js
@@ -132,7 +132,7 @@ $(function () {
             data: params,
             url: "//dev.markitondemand.com/Api/v2/InteractiveChart/jsonp",
             headers: { 
-                'Origin': '//dev.markitondemand.com/MODApis'
+                'Origin': 'http://dev.markitondemand.com/MODApis'
             },
             dataType: "jsonp",
             context: this,
@@ -382,7 +382,7 @@ $(function () {
             dataType: "jsonp",
             context: this,
             headers: { 
-                'Origin': '//dev.markitondemand.com/MODApis'
+                'Origin': 'http://dev.markitondemand.com/MODApis'
             },
             success: function(json){
                 //Catch errors
